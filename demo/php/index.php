c6391ad010ed8920cc8b1fb4581661e2c8399c00
Stephen Dolier <stephen@dolier.net>
2015-02-23 19:18:30 +1100

    Added ip address policy match

--- demo/php/index.php
+++ demo/php/index.php
@@ -4,11 +4,11 @@ date_default_timezone_set('UTC');
 
 $policyJson = "{"
    ."\"Statement\": [{"
-      ."\"Resource\":\"http://localhost:8099/private/success.jpg\","
+      ."\"Resource\":\"//localhost/private/success.jpg\","
       ."\"Condition\":{"
          ."\"DateLessThan\":{\"Apache:EpochTime\":" . (time()+(10*60)) . "},"
          ."\"DateGreaterThan\":{\"Apache:EpochTime\":" . (time()-(1*60)) . "},"
-         ."\"IpAddress\":{\"Apache:SourceIp\":\"127.0.0.1\"}"
+         ."\"IpAddress\":{\"Apache:SourceIp\":\"::1\"}"
       ."}"
    ."}]"
 ."}";
